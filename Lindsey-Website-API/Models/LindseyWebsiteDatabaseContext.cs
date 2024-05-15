ec352e72f063d69d61e41b0897e141bb59e13d9f
Peyton Comer <pcomer2@huskers.unl.edu>
2024-05-15 13:31:38 -0500

    some more initializations

---
+++ Lindsey-Website-API/Models/LindseyWebsiteDatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lindsey_Website_API.Models;
+
+public partial class LindseyWebsiteDatabaseContext : DbContext
+{
+    public LindseyWebsiteDatabaseContext()
+    {
+    }
+
+    public LindseyWebsiteDatabaseContext(DbContextOptions<LindseyWebsiteDatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-GP97G8K;Database=Lindsey-Website-Database;Trusted_Connection=True;Encrypt=false");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- Lindsey-Website-API/obj/Debug/net8.0/Lindsey-Website-API.AssemblyInfo.cs
+++ Lindsey-Website-API/obj/Debug/net8.0/Lindsey-Website-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lindsey-Website-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75e7d104ffc2eb83d9e08925be7eb23b05dcd4df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bea2aa57e8e71af4421c9fad0ddfdef9b7e0aeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lindsey-Website-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lindsey-Website-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
